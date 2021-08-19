212b397c4205515fc1eabfcccd640e688efe1b53
Blujedis <blujedicorp@gmail.com>
2021-08-18 20:37:07 -0700

    add exit in cli

--- dist/cli/cli.js
+++ dist/cli/cli.js
@@ -105,6 +105,9 @@ ${utils_1.colorize('Examples:', 'cyanBright')}
   ${appNameLower} set active null (or empty or none to reset to []) 
   ${appNameLower} set reverts "migration_one,migration_two" down
 `;
+function exit(code = 0) {
+    process.exit(code);
+}
 function handleResult(result) {
     let names = !result.names.length ? 'none' : result.names;
     if (Array.isArray(names))
@@ -122,6 +125,7 @@ function handleResult(result) {
     console.log(`  direction: ${result.type}`);
     console.log(`  migrations: ${names}`);
     console.log(`  count: ${result.count}\n`);
+    exit();
 }
 function listen() {
     return __awaiter(this, void 0, void 0, function* () {
@@ -137,56 +141,59 @@ function listen() {
             console.log(`${log_symbols_1.default.warning} ${utils_1.APP_PKG.name} initialized without config.js`);
         }
         const mygra = new mygra_1.Mygra(userConfig);
-        if (cmd === 'help') {
-            console.log(help);
-        }
-        if (cmd === 'examples') {
-            console.log(examples);
-        }
-        else if (cmd === 'config') {
-            console.log(config.props);
-        }
-        else if (cmd === 'get') {
-            console.log(config.get(argv._[1]));
-        }
-        else if (cmd === 'set') {
-            // const current = config.props;
-            const key = argv._[1];
-            let val = argv._[2];
-            const val2 = argv._[3];
-            if (key === 'active' || key === 'reverts') {
-                if (val === 'null' || val === 'none' || val === 'empty') {
-                    val = [];
+        if (['help', 'examples', 'config', 'get', 'set', 'init'].includes(cmd)) {
+            if (cmd === 'help') {
+                console.log(help);
+            }
+            if (cmd === 'examples') {
+                console.log(examples);
+            }
+            else if (cmd === 'config') {
+                console.log(config.props);
+            }
+            else if (cmd === 'get') {
+                console.log(config.get(argv._[1]));
+            }
+            else if (cmd === 'set') {
+                // const current = config.props;
+                const key = argv._[1];
+                let val = argv._[2];
+                const val2 = argv._[3];
+                if (key === 'active' || key === 'reverts') {
+                    if (val === 'null' || val === 'none' || val === 'empty') {
+                        val = [];
+                    }
+                    else if (!val2) {
+                        console.log(`Set ${key} using format "mygra set ${key} migration_name up"`);
+                        process.exit();
+                    }
+                    else {
+                        if (key === 'reverts')
+                            val = val.split(',').map(v => v.trim());
+                        val = [val, val2];
+                    }
                 }
-                else if (!val2) {
-                    console.log(`Set ${key} using format "mygra set ${key} migration_name up"`);
-                    process.exit();
+                else if (key === 'directory') {
+                    val = val || utils_1.MYGRA_DEFAULT_PATH;
                 }
+                config.set(key, val);
+            }
+            else if (cmd === 'init') {
+                const srcPath = path_1.join(__dirname, '../../blueprints');
+                const targetPath = path_1.join(cwd, appNameLower);
+                if (!fs_extra_1.existsSync(targetPath) || argv.force) {
+                    fs_extra_1.copySync(srcPath, targetPath);
+                    console.log(log_symbols_1.default.success, `successfully initialized ${appName}`);
+                }
+                // already exists merge instead
+                // don't overwrite config just 
+                // merge/copy templates
                 else {
-                    if (key === 'reverts')
-                        val = val.split(',').map(v => v.trim());
-                    val = [val, val2];
+                    fs_extra_1.copySync(path_1.join(__dirname, '../../blueprints/templates'), path_1.join(cwd, appNameLower, 'templates'));
+                    console.log(log_symbols_1.default.success, `successfully reinitialized ${appName}, config was NOT overwritten use --force to overwrite`);
                 }
             }
-            else if (key === 'directory') {
-                val = val || utils_1.MYGRA_DEFAULT_PATH;
-            }
-            config.set(key, val);
-        }
-        else if (cmd === 'init') {
-            const srcPath = path_1.join(__dirname, '../../blueprints');
-            const targetPath = path_1.join(cwd, appNameLower);
-            if (!fs_extra_1.existsSync(targetPath) || argv.force) {
-                fs_extra_1.copySync(srcPath, targetPath);
-                console.log(log_symbols_1.default.success, `successfully initialized ${appName}`);
-            }
-            // already exists merge instead
-            // don't overwrite config just 
-            // merge/copy templates
-            else {
-                fs_extra_1.copySync(path_1.join(__dirname, '../../blueprints/templates'), path_1.join(cwd, appNameLower, 'templates'));
-                console.log(log_symbols_1.default.success, `successfully reinitialized ${appName}, config was NOT overwritten use --force to overwrite`);
-            }
+            exit();
         }
         else if (!fs_extra_1.existsSync(config.get('directory'))) {
             const initCmd = `${appNameLower} init`;
@@ -194,84 +201,94 @@ function listen() {
             console.log(`\n${log_symbols_1.default.warning} ${appName} directory NOT found or has moved, please initialize.\n`);
             console.log(`  Run ${utils_1.colorize(initCmd, 'cyan')} or ${utils_1.colorize(setCmd, 'cyan')}\n`);
         }
-        else if (cmd === 'list') {
-            let filenames = yield mygra.getFilenames();
-            filenames = filenames.map(file => utils_1.getBaseName(file));
-            console.log('\n' + filenames.join('\n') + '\n');
-        }
-        else if (cmd === 'show') {
-            const files = yield mygra.getFilenames();
-            const found = utils_1.findIndex(files, argv._[1]);
-            if (found === -1) {
-                console.log(`Migration ${argv._[1]} NOT found`);
+        else {
+            if (cmd === 'list') {
+                let filenames = yield mygra.getFilenames();
+                filenames = filenames.map(file => utils_1.getBaseName(file));
+                console.log('\n' + filenames.join('\n') + '\n');
+                exit();
             }
-            else {
-                const filename = files[found];
-                const imported = yield mygra.import(filename);
-                const parsed = path_1.parse(filename);
-                const hasMethod = argv.up || argv.down;
-                const showStates = {
-                    up: !hasMethod || argv.up,
-                    down: !hasMethod || argv.down
-                };
-                console.log(`\n----------------------------------------------`);
-                console.log(` ${utils_1.colorize(parsed.name, 'whiteBright')}`);
-                if (imported.description)
-                    console.log(` ${utils_1.colorize(imported.description, 'dim')}`);
-                console.log(`----------------------------------------------\n`);
-                if (showStates.up) {
-                    console.log(utils_1.colorize((imported.up || '').toString(), 'greenBright'));
-                    console.log();
+            else if (cmd === 'show') {
+                const files = yield mygra.getFilenames();
+                if (!argv._[1]) {
+                    console.log(`Cannot show Migration of undefined.`);
+                    exit();
                 }
-                if (showStates.down) {
-                    console.log(utils_1.colorize((imported.down || '').toString(), 'redBright'));
-                    console.log();
+                const found = utils_1.findIndex(files, argv._[1]);
+                if (found === -1) {
+                    console.log(`Migration ${argv._[1]} NOT found`);
+                    exit();
+                }
+                else {
+                    const filename = files[found];
+                    const imported = yield mygra.import(filename);
+                    const parsed = path_1.parse(filename);
+                    const hasMethod = argv.up || argv.down;
+                    const showStates = {
+                        up: !hasMethod || argv.up,
+                        down: !hasMethod || argv.down
+                    };
+                    console.log(`\n----------------------------------------------`);
+                    console.log(` ${utils_1.colorize(parsed.name, 'whiteBright')}`);
+                    if (imported.description)
+                        console.log(` ${utils_1.colorize(imported.description, 'dim')}`);
+                    console.log(`----------------------------------------------\n`);
+                    if (showStates.up) {
+                        console.log(utils_1.colorize((imported.up || '').toString(), 'greenBright'));
+                        console.log();
+                    }
+                    if (showStates.down) {
+                        console.log(utils_1.colorize((imported.down || '').toString(), 'redBright'));
+                        console.log();
+                    }
+                    exit();
                 }
             }
-        }
-        else if (cmd === 'create') {
-            const { _, $0 } = argv, rest = __rest(argv, ["_", "$0"]);
-            const result = yield mygra.create(Object.assign({ name: _[1] }, rest));
-            const symbol = result.ok ? log_symbols_1.default.success : log_symbols_1.default.error;
-            if (result.ok) {
-                console.log(symbol, result.message);
+            else if (cmd === 'create') {
+                const { _, $0 } = argv, rest = __rest(argv, ["_", "$0"]);
+                const result = yield mygra.create(Object.assign({ name: _[1] }, rest));
+                const symbol = result.ok ? log_symbols_1.default.success : log_symbols_1.default.error;
+                if (result.ok) {
+                    console.log(symbol, result.message);
+                }
+                else {
+                    console.error(symbol, result.message);
+                }
+                exit();
             }
-            else {
-                console.error(symbol, result.message);
-            }
-        }
-        else if (cmd === 'up') {
-            const val = argv._[1] === 'all' ? '*' : argv._[1];
-            const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val);
-            const result = yield mygra.up(nameOrLevel, argv.preview);
-            handleResult(result);
-        }
-        else if (cmd === 'down') {
-            const val = argv._[1] === 'all' ? '*' : argv._[1];
-            const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val);
-            const result = yield mygra.down(nameOrLevel, argv.preview);
-            handleResult(result);
-        }
-        else if (cmd === 'reset') {
-            const result = yield mygra.reset(argv.preview);
-            handleResult(result);
-        }
-        else if (cmd === 'revert') {
-            const [reverts, dir] = mygra.reverts || [];
-            if (!(reverts === null || reverts === void 0 ? void 0 : reverts.length)) {
-                console.log(log_symbols_1.default.warning, `Cannot revert with migration steps of undefined`);
-            }
-            else {
-                // Rebuild filenames maybe not necessary but
-                // by storing reverts by name only allows moving
-                // directory, won't break basically.
-                const filenames = reverts.map(name => `${mygra.directory}/migrations/${name}${mygra.extension}`);
-                const migrations = yield mygra.load(filenames);
-                const result = yield mygra.revert(migrations, dir);
+            else if (cmd === 'up') {
+                const val = argv._[1] === 'all' ? '*' : argv._[1];
+                const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val);
+                const result = yield mygra.up(nameOrLevel, argv.preview);
                 handleResult(result);
             }
+            else if (cmd === 'down') {
+                const val = argv._[1] === 'all' ? '*' : argv._[1];
+                const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val);
+                const result = yield mygra.down(nameOrLevel, argv.preview);
+                handleResult(result);
+            }
+            else if (cmd === 'reset') {
+                const result = yield mygra.reset(argv.preview);
+                handleResult(result);
+            }
+            else if (cmd === 'revert') {
+                const [reverts, dir] = mygra.reverts || [];
+                if (!(reverts === null || reverts === void 0 ? void 0 : reverts.length)) {
+                    console.log(log_symbols_1.default.warning, `Cannot revert with migration steps of undefined`);
+                    exit();
+                }
+                else {
+                    // Rebuild filenames maybe not necessary but
+                    // by storing reverts by name only allows moving
+                    // directory, won't break basically.
+                    const filenames = reverts.map(name => `${mygra.directory}/migrations/${name}${mygra.extension}`);
+                    const migrations = yield mygra.load(filenames);
+                    const result = yield mygra.revert(migrations, dir);
+                    handleResult(result);
+                }
+            }
         }
-        process.exit();
     });
 }
 process.on('uncaughtException', (err) => {
--- src/cli.ts
+++ src/cli.ts
@@ -67,7 +67,11 @@ ${colorize('Examples:', 'cyanBright')}
   ${appNameLower} set active some_migration up 
   ${appNameLower} set active null (or empty or none to reset to []) 
   ${appNameLower} set reverts "migration_one,migration_two" down
-`
+`;
+
+function exit(code = 0) {
+  process.exit(code);
+}
 
 function handleResult(result: IMigrationResult) {
   let names = !result.names.length ? 'none' : result.names;
@@ -86,6 +90,7 @@ function handleResult(result: IMigrationResult) {
   console.log(`  direction: ${result.type}`);
   console.log(`  migrations: ${names}`);
   console.log(`  count: ${result.count}\n`);
+  exit();
 }
 
 async function listen() {
@@ -106,61 +111,67 @@ async function listen() {
 
   const mygra = new Mygra(userConfig);
 
-  if (cmd === 'help') {
-    console.log(help);
-  }
+  if (['help', 'examples', 'config', 'get', 'set', 'init'].includes(cmd)) {
 
-  if (cmd === 'examples') {
-    console.log(examples);
-  }
+    if (cmd === 'help') {
+      console.log(help);
+    }
 
-  else if (cmd === 'config') {
-    console.log(config.props);
-  }
+    if (cmd === 'examples') {
+      console.log(examples);
+    }
 
-  else if (cmd === 'get') {
-    console.log(config.get(argv._[1]));
-  }
+    else if (cmd === 'config') {
+      console.log(config.props);
+    }
 
-  else if (cmd === 'set') {
-    // const current = config.props;
-    const key = argv._[1];
-    let val = argv._[2] as any;
-    const val2 = argv._[3];
-    if (key === 'active' || key === 'reverts') {
-      if (val === 'null' || val === 'none' || val === 'empty') {
-        val = [];
+    else if (cmd === 'get') {
+      console.log(config.get(argv._[1]));
+    }
+
+    else if (cmd === 'set') {
+      // const current = config.props;
+      const key = argv._[1];
+      let val = argv._[2] as any;
+      const val2 = argv._[3];
+      if (key === 'active' || key === 'reverts') {
+        if (val === 'null' || val === 'none' || val === 'empty') {
+          val = [];
+        }
+        else if (!val2) {
+          console.log(`Set ${key} using format "mygra set ${key} migration_name up"`);
+          process.exit();
+        }
+        else {
+          if (key === 'reverts')
+            val = val.split(',').map(v => v.trim());
+          val = [val, val2];
+        }
       }
-      else if (!val2) {
-        console.log(`Set ${key} using format "mygra set ${key} migration_name up"`);
-        process.exit();
+      else if (key === 'directory') {
+        val = val || MYGRA_DEFAULT_PATH;
       }
+      config.set(key, val);
+    }
+
+    else if (cmd === 'init') {
+      const srcPath = join(__dirname, '../../blueprints');
+      const targetPath = join(cwd, appNameLower);
+      if (!existsSync(targetPath) || argv.force) {
+        copySync(srcPath, targetPath);
+        console.log(symbols.success, `successfully initialized ${appName}`);
+      }
+      // already exists merge instead
+      // don't overwrite config just 
+      // merge/copy templates
       else {
-        if (key === 'reverts')
-          val = val.split(',').map(v => v.trim());
-        val = [val, val2];
+        copySync(join(__dirname, '../../blueprints/templates'), join(cwd, appNameLower, 'templates'));
+        console.log(symbols.success, `successfully reinitialized ${appName}, config was NOT overwritten use --force to overwrite`);
       }
     }
-    else if (key === 'directory') {
-      val = val || MYGRA_DEFAULT_PATH;
-    }
-    config.set(key, val);
-  }
 
-  else if (cmd === 'init') {
-    const srcPath = join(__dirname, '../../blueprints');
-    const targetPath = join(cwd, appNameLower);
-    if (!existsSync(targetPath) || argv.force) {
-      copySync(srcPath, targetPath);
-      console.log(symbols.success, `successfully initialized ${appName}`);
-    }
-    // already exists merge instead
-    // don't overwrite config just 
-    // merge/copy templates
-    else {
-      copySync(join(__dirname, '../../blueprints/templates'), join(cwd, appNameLower, 'templates'));
-      console.log(symbols.success, `successfully reinitialized ${appName}, config was NOT overwritten use --force to overwrite`);
-    }
+    exit();
+
   }
 
   else if (!existsSync(config.get('directory'))) {
@@ -170,95 +181,111 @@ async function listen() {
     console.log(`  Run ${colorize(initCmd, 'cyan')} or ${colorize(setCmd, 'cyan')}\n`);
   }
 
-  else if (cmd === 'list') {
-    let filenames = await mygra.getFilenames();
-    filenames = filenames.map(file => getBaseName(file));
-    console.log('\n' + filenames.join('\n') + '\n');
-  }
+  else {
 
-  else if (cmd === 'show') {
-    const files = await mygra.getFilenames();
-    const found = findIndex(files, argv._[1]) as any;
-    if (found === -1) {
-      console.log(`Migration ${argv._[1]} NOT found`);
+
+    if (cmd === 'list') {
+      let filenames = await mygra.getFilenames();
+      filenames = filenames.map(file => getBaseName(file));
+      console.log('\n' + filenames.join('\n') + '\n');
+      exit();
     }
-    else {
-      const filename = files[found];
-      const imported = await mygra.import(filename);
-      const parsed = parse(filename);
-      const hasMethod = argv.up || argv.down;
-      const showStates = {
-        up: !hasMethod || argv.up,
-        down: !hasMethod || argv.down
-      };
-      console.log(`\n----------------------------------------------`);
-      console.log(` ${colorize(parsed.name, 'whiteBright')}`);
-      if (imported.description)
-        console.log(` ${colorize(imported.description, 'dim')}`);
-      console.log(`----------------------------------------------\n`);
-      if (showStates.up) {
-        console.log(colorize((imported.up || '').toString(), 'greenBright'));
-        console.log();
+
+    else if (cmd === 'show') {
+      const files = await mygra.getFilenames();
+      if (!argv._[1]) {
+        console.log(`Cannot show Migration of undefined.`);
+        exit();
       }
-      if (showStates.down) {
-        console.log(colorize((imported.down || '').toString(), 'redBright'));
-        console.log();
+      const found = findIndex(files, argv._[1]) as any;
+      if (found === -1) {
+        console.log(`Migration ${argv._[1]} NOT found`);
+        exit();
+      }
+      else {
+        const filename = files[found];
+        const imported = await mygra.import(filename);
+        const parsed = parse(filename);
+        const hasMethod = argv.up || argv.down;
+        const showStates = {
+          up: !hasMethod || argv.up,
+          down: !hasMethod || argv.down
+        };
+        console.log(`\n----------------------------------------------`);
+        console.log(` ${colorize(parsed.name, 'whiteBright')}`);
+        if (imported.description)
+          console.log(` ${colorize(imported.description, 'dim')}`);
+        console.log(`----------------------------------------------\n`);
+        if (showStates.up) {
+          console.log(colorize((imported.up || '').toString(), 'greenBright'));
+          console.log();
+        }
+        if (showStates.down) {
+          console.log(colorize((imported.down || '').toString(), 'redBright'));
+          console.log();
+        }
+
+        exit();
+
       }
 
     }
-  }
 
-  else if (cmd === 'create') {
-    const { _, $0, ...rest } = argv;
-    const result = await mygra.create({
-      name: _[1],
-      ...rest
-    });
-    const symbol = result.ok ? symbols.success : symbols.error;
-    if (result.ok) {
-      console.log(symbol, result.message);
+    else if (cmd === 'create') {
+      const { _, $0, ...rest } = argv;
+      const result = await mygra.create({
+        name: _[1],
+        ...rest
+      });
+      const symbol = result.ok ? symbols.success : symbols.error;
+      if (result.ok) {
+        console.log(symbol, result.message);
+      }
+      else {
+        console.error(symbol, result.message);
+      }
+      exit();
     }
-    else {
-      console.error(symbol, result.message);
-    }
-  }
 
-  else if (cmd === 'up') {
-    const val = argv._[1] === 'all' ? '*' : argv._[1];
-    const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val) as string | number;
-    const result = await mygra.up(nameOrLevel, argv.preview);
-    handleResult(result);
-  }
-
-  else if (cmd === 'down') {
-    const val = argv._[1] === 'all' ? '*' : argv._[1];
-    const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val) as string | number;
-    const result = await mygra.down(nameOrLevel, argv.preview);
-    handleResult(result);
-  }
-
-  else if (cmd === 'reset') {
-    const result = await mygra.reset(argv.preview);
-    handleResult(result);
-  }
-
-  else if (cmd === 'revert') {
-    const [reverts, dir] = mygra.reverts || [];
-    if (!reverts?.length) {
-      console.log(symbols.warning, `Cannot revert with migration steps of undefined`);
-    }
-    else {
-      // Rebuild filenames maybe not necessary but
-      // by storing reverts by name only allows moving
-      // directory, won't break basically.
-      const filenames = reverts.map(name => `${mygra.directory}/migrations/${name}${mygra.extension}`)
-      const migrations = await mygra.load(filenames);
-      const result = await mygra.revert(migrations, dir);
+    else if (cmd === 'up') {
+      const val = argv._[1] === 'all' ? '*' : argv._[1];
+      const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val) as string | number;
+      const result = await mygra.up(nameOrLevel, argv.preview);
       handleResult(result);
     }
+
+    else if (cmd === 'down') {
+      const val = argv._[1] === 'all' ? '*' : argv._[1];
+      const nameOrLevel = (/^\d+$/.test(val) ? parseInt(val) : val) as string | number;
+      const result = await mygra.down(nameOrLevel, argv.preview);
+      handleResult(result);
+    }
+
+    else if (cmd === 'reset') {
+      const result = await mygra.reset(argv.preview);
+      handleResult(result);
+    }
+
+    else if (cmd === 'revert') {
+      const [reverts, dir] = mygra.reverts || [];
+      if (!reverts?.length) {
+        console.log(symbols.warning, `Cannot revert with migration steps of undefined`);
+        exit();
+      }
+      else {
+        // Rebuild filenames maybe not necessary but
+        // by storing reverts by name only allows moving
+        // directory, won't break basically.
+        const filenames = reverts.map(name => `${mygra.directory}/migrations/${name}${mygra.extension}`)
+        const migrations = await mygra.load(filenames);
+        const result = await mygra.revert(migrations, dir);
+        handleResult(result);
+      }
+
+    }
+
   }
 
-  process.exit();
 
 }
 
